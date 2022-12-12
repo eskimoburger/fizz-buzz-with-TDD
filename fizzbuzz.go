de34c8f35c04e7ddce67def700d3f041f6bf278e
Eskimoburger <soontornp60@nu.ac.th>
2022-12-13 01:15:55 +0700

    Refactoring #1

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,11 +1,10 @@
 package fizzbuzz
 
+import "fmt"
+
 func FizzBuzz(number int) string {
 	if number == 3 {
 		return "Fizz"
 	}
-	if number == 2 {
-		return "2"
-	}
-	return "1"
+	return fmt.Sprintf("%d", number)
 }
