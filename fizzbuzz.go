3e20b2b0edf9f657ebb57104b620c428526ee8bd
Eskimoburger <soontornp60@nu.ac.th>
2022-12-13 01:29:09 +0700

    Input 6 should be Fizz

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -6,7 +6,7 @@ func FizzBuzz(number int) string {
 	if number == 5 {
 		return "Buzz"
 	}
-	if number == 3 {
+	if number == 3 || number == 6 {
 		return "Fizz"
 	}
 	return fmt.Sprintf("%d", number)
