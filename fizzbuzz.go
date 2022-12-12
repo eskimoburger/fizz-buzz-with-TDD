55911b1803066c043f677cfce29fef47352f8764
Eskimoburger <soontornp60@nu.ac.th>
2022-12-13 01:12:37 +0700

    Input 3 shouldbe Fizz

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,10 +1,11 @@
 package fizzbuzz
 
 func FizzBuzz(number int) string {
-
+	if number == 3 {
+		return "Fizz"
+	}
 	if number == 2 {
 		return "2"
-
 	}
 	return "1"
 }
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -20,3 +20,21 @@ func TestInput2ShouldReturn2(t *testing.T) {
 	}
 
 }
+func TestInput3ShouldReturnFizz(t *testing.T) {
+	want := "Fizz"
+	input := 3
+	got := FizzBuzz(input)
+	if got != want {
+		t.Errorf("FizzBuzz(%d) = %q want %q", input, got, want)
+	}
+
+}
+func TestInput4ShouldReturn4(t *testing.T) {
+	want := "4"
+	input := 4
+	got := FizzBuzz(input)
+	if got != want {
+		t.Errorf("FizzBuzz(%d) = %q want %q", input, got, want)
+	}
+
+}
